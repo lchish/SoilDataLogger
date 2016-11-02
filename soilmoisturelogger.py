c7ae8d834e4288a760414b0b403825f6225e531c
Leslie Chisholm <lesliec@wdtl.com>
2016-11-03 11:50:05 +1300

    Minor fixes

--- soilmoisturelogger.py
+++ soilmoisturelogger.py
@@ -3,28 +3,21 @@
 import serial
 import time
 import os
-import time 
+import time
 from time import sleep
 from datetime import datetime
 
-ser = serial.Serial('/dev/ttyACM2', 9600)
+ser = serial.Serial('/dev/ttyACM2', 9600, timeout=10)
 
-file = open("data_log.csv", "a")
-
-#varl = 'temp'
-
-if os.stat("data_log.csv").st_size == 0:
+with open("data_log.csv", "a") as file:
+    if os.stat("data_log.csv").st_size == 0:
         file.write("Time,Sensor1\n")
-while True:
-		varl = ser.readline()
-		time.sleep(1)
-		varl = varl.decode('utf-8')
-		#varl = varl.replace("\r'\n'",'')
-		#varl = varl.replace("b'",'')
-		now = datetime.now()
-		print("Writting Reading - "+str(now))
-		file.write(time	.strftime('%l:%M%p on %b %d %Y')+","+str(varl)+","+"\n")
-		file.flush()
-		time.sleep(60)
-		#file.close()
-		
+
+    while True:
+        varl = ser.readline()
+        varl = varl.decode('utf-8')
+        #varl = varl.replace("\r'\n'",'')
+        #varl = varl.replace("b'",'')
+        print("Writing Reading - " + str(datetime.now()))
+        file.write(time.strftime('%l:%M%p on %b %d %Y') + "," + str(varl) + "," + "\n")
+        file.flush()
