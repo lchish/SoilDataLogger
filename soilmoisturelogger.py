e238cad3eb9fed7efa61a0093757b67017350b75
Leslie Chisholm <lesliec@wdtl.com>
2016-11-03 13:31:11 +1300

    Handle serial port timeouts gracefully

--- soilmoisturelogger.py
+++ soilmoisturelogger.py
@@ -7,7 +7,10 @@ import time
 from time import sleep
 from datetime import datetime
 
-ser = serial.Serial('/dev/ttyACM2', 9600, timeout=10)
+port = '/dev/ttyACM2'
+timeout = 10
+
+ser = serial.Serial(port, 9600, timeout=timeout)
 
 with open("data_log.csv", "a") as file:
     if os.stat("data_log.csv").st_size == 0:
@@ -15,7 +18,10 @@ with open("data_log.csv", "a") as file:
 
     while True:
         varl = ser.readline().decode('utf-8')
-        #varl = varl.replace("\r'\n'",'')
-        #varl = varl.replace("b'",'')
-        print("Writing Reading - " + str(datetime.now()))
-        file.write(time.strftime('%l:%M%p on %b %d %Y') + "t" + str(varl) + "\n")
+        if len(varl) > 0:
+            #varl = varl.replace("\r'\n'",'')
+            #varl = varl.replace("b'",'')
+            print("Writing Reading - " + str(datetime.now()))
+            file.write(time.strftime('%l:%M%p on %b %d %Y') + "\t" + str(varl) + "\n")
+        else:
+            file.write(time.strftime('%l:%M%p on %b %d %Y') + "\t" + str(timeout) + "second timeout reading from serial " + port + "\n")
